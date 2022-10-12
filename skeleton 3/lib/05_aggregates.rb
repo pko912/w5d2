96ce690cc6df3360d9ef47262866fa283a5728d2
pko912 <kopeter912@gmail.com>
2022-10-12 11:01:54 -0400

    sql

--- skeleton 3/lib/05_aggregates.rb
+++ skeleton 3/lib/05_aggregates.rb
@@ -68,6 +68,13 @@ end
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+  SELECT
+    continent, count(name)
+  FROM
+    countries
+  GROUP BY 
+    continent;
+
   SQL
 end
 
@@ -75,11 +82,27 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+  SELECT
+    continent, count(*)
+  FROM
+    countries
+  WHERE
+    population >= 10000000
+  GROUP BY
+    continent;
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+  SELECT
+    continent
+  FROM
+    countries
+  GROUP BY
+    continent
+  HAVING
+    sum(population) > 100000000;
   SQL
 end
